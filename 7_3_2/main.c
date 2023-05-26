38259c1d698325aaf8bc7202aa2a43a91479e724
pjastr <24464854+pjastr@users.noreply.github.com>
2023-05-26 23:53:25 +0200

    7_3_2

---
+++ 7_3_2/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element
+{
+    int i;
+    struct element *next;
+};
+
+//funkcja dodatkowo ustawia listê jako pust¹
+struct element *wyczysc(struct element * lista)
+{
+    struct element *wsk = lista;
+    struct element * wsk2 = wsk;
+    while(wsk != NULL)
+    {
+        wsk= wsk->next;
+        free(wsk2);
+        wsk2 = wsk;
+    }
+    return NULL;
+}
+
+void wyswietlListeBezGlowy(struct element *lista)
+{
+    if (lista == NULL)
+    {
+        printf("Lista jest pusta.\n---\n");
+        return;
+    }
+    struct element *wsk = lista;
+    while (wsk != NULL)
+    {
+        printf("%d\n", wsk->i);
+        wsk = wsk->next;
+    }
+    printf("---\n");
+}
+
+int main()
+{
+    struct element *lista = malloc(sizeof(struct element));
+    lista->i = 1;
+    lista->next = malloc(sizeof(struct element));
+    lista->next->i = -72;
+    lista->next->next = malloc(sizeof(struct element));
+    lista->next->next->i = 3;
+    lista->next->next->next = NULL;    wyswietlListeBezGlowy(lista);
+    lista = wyczysc(lista);
+    wyswietlListeBezGlowy(lista);
+    return 0;
+}
+
