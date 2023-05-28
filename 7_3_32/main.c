6635c89b180c86deca7961ec23d4ef15a934780a
pjastr <24464854+pjastr@users.noreply.github.com>
2023-05-28 11:49:30 +0200

    7_3_32

---
+++ 7_3_32/main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element
+{
+    int i;
+    struct element *next;
+};
+
+struct element *polacz_listy(struct element *lista1, struct element *lista2)
+{
+    //mocno korzystam z zalozenia ze listy s¹ takiej samej dlugosci
+    if (lista1 == NULL)
+    {
+        return NULL;
+    }
+    // nowa lista
+    struct element * new_list = lista1;
+    // wskaznik do przechodzenia po nowej liscie
+    struct element * wsk = new_list;
+    // wskazniki do przechodzenia po listach 1 i 2
+    struct element * wsk1 = lista1;
+    struct element * wsk2 = lista2;
+    // teraz pêtla
+    while(wsk1 != NULL)
+    {
+        wsk1 = wsk1->next;
+        wsk->next = wsk2;
+        wsk = wsk->next;
+        wsk2 = wsk2->next;
+        wsk->next = wsk1;
+        wsk = wsk->next;
+    }
+    return new_list;
+}
+
+void wyswietlListeBezGlowy(struct element *lista)
+{
+    if (lista == NULL)
+    {
+        printf("Lista jest pusta.\n---\n");
+        return;
+    }
+    struct element *wsk = lista;
+    while (wsk != NULL)
+    {
+        printf("%d:%p\n", wsk->i, wsk);
+        wsk = wsk->next;
+    }
+    printf("---\n");
+}
+
+int main()
+{
+    struct element *lista1 = malloc(sizeof(struct element));
+    lista1->i = 1;
+    lista1->next = malloc(sizeof(struct element));
+    lista1->next->i = 2;
+    lista1->next->next = malloc(sizeof(struct element));
+    lista1->next->next->i = 3;
+    lista1->next->next->next = NULL;
+    struct element *lista2 = malloc(sizeof(struct element));
+    lista2->i = -5;
+    lista2->next = malloc(sizeof(struct element));
+    lista2->next->i = -6;
+    lista2->next->next = malloc(sizeof(struct element));
+    lista2->next->next->i = -7;
+    lista2->next->next->next = NULL;
+    wyswietlListeBezGlowy(lista1);
+    wyswietlListeBezGlowy(lista2);
+    struct element *lista3 = polacz_listy(lista1, lista2);
+    wyswietlListeBezGlowy(lista3);
+    // w tym zadaniu pierwotne listy nie s¹ ju¿ przechowywane nigdzie w pamieci w pierwotnej kolejnosci
+}
+
